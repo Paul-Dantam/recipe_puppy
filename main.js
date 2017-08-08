441a5331fcef3f28813314e4c3c90b00e76c3814
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-08 15:38:10 -0500

    fixed search and background images

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let recipeSearch = document.querySelector("input");
 let recipeContainer = document.querySelector(".container");
 
-let url = "http://www.recipepuppy.com/api/?" + recipeSearch.value;
+recipeSearch.addEventListener("keydown", function(e) {
+  let url =
+    "http://recipepuppyproxy.herokuapp.com/api/?q=" + recipeSearch.value;
 
-axios.get(url).then(function(response) {
-  let data = response.results;
+  if (event.keyCode === 13) {
+    axios.get(url).then(function(response) {
+      let data = response.data.results;
+      console.log(data);
+      data.forEach(function(data) {
+        let searchResults = ``;
+        searchResults = `
+            <div class="recipe" style="background-image: url(${data.thumbnail}), url(https://www.placecage.com/c/140/200)"><p class="recipeTitle"><a href="${data.href}">${data.title}</a></p>
+            </div>`;
 
-  data.forEach(function() {
-    recipeContainer += `
-            <a href="${data.href}"<div class="recipe" style ="background:url${data.thumbnail}">
-                <p class="recipeTitle">${data.title}</p>
-            </div></a>`;
-  });
+        recipeContainer.innerHTML += searchResults;
+      });
+    });
+  }
 });
